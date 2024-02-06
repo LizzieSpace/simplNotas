b63d3747584de9cbee24c82e4f67d883ecc0b929
AliCCFire <alicecfire@gmail.com>
2024-02-06 18:31:39 -0300

    add media e rendimento
    - rep_faltas - funcional
    - media - funcional
    - rendimento - funcional
    
    Signed-off-by: AliCCFire <alicecfire@gmail.com>

--- simplNotas.py
+++ simplNotas.py
@@ -11,7 +11,6 @@ class simplNotas:
 	def __init__(self, path_notas: str, path_faltas: str = ...) -> None:
 		self._notas = downcast(pd.read_fwf(path_notas, index_col=0))
 		self.notas = pd.DataFrame = self._notas
-		self.media: pd.DataFrame = ...
 		self._faltas = downcast(
 			pd.read_fwf(path_faltas, header=None, index_col=0)
 			).infer_objects() if path_faltas is not None else None
@@ -20,10 +19,15 @@ class simplNotas:
 		"""
 		:return: True se reprovado
 		"""
+		reprovado = False
 		counts = self._faltas.loc[nome_aluno].value_counts()
-		return True if counts[False] / counts.sum() > .25 else False
+		match counts.size:
+			case 1: reprovado = not counts.index[0]
+			case 2: reprovado = False if counts[True] / counts.sum() > .75 else True
+			case _: raise KeyError("Os valores na tabela de faltas está correta? ")
+		return reprovado
 
-	def media(self, pesos: dict = ...):# TODO: Testar media e resolver erros
+	def get_media(self, pesos: dict = ...):
 		"""
 		Calcula a media ponderada para todos na turma.\n
 		{<colname>: <value>,...} para cada coluna na média
@@ -35,27 +39,29 @@ class simplNotas:
 			for Pn in self._notas.columns:
 				pesos[Pn] = 1
 		try:
-			sum_p = pd.Series([0 for nome in self._notas.index])
+			sum_p = pd.Series([0 for i in self._notas.index], index=self._notas.index, name="media")
 			for nome, peso in pesos.items():
 				sum_p += self._notas[nome] * peso
-			self.media = sum_p / sum(pesos.values())
-			return self.media
+			return sum_p / sum(pesos.values())
 		except KeyError:
 			print(KeyError)
 			return None
 
-	def rendimento(self): #TODO terminar rendimento
-
-		for nome, nota in self.media.items():
+	def rendimento(self, notas_finais: pd.Series):
+		rendimento = pd.Series([], name="rendimento")
+		for nome, nota in notas_finais.items():
 			match nota:
-				case _ if 10 >= nota >= 9.0: self.notas["rendimento"][nome] = "SS"
-				case _ if 9.0 > nota >= 7.0: self.notas["rendimento"] = "MS"
-				case _ if 7.0 > nota >= 5.0: self.notas["rendimento"] = "MM"
-				case _ if 5.0 > nota >= 3.0: self.notas["rendimento"] = "MI"
-				case _ if 3.0 > nota > 0: self.notas["rendimento"] = "II"
-				case _ if nota == 0: self.notas["rendimento"] = "SR"
+				case _ if 10 >= nota >= 9.0: rendimento[nome] = "SS"
+				case _ if 9.0 > nota >= 7.0: rendimento[nome] = "MS"
+				case _ if 7.0 > nota >= 5.0: rendimento[nome] = "MM"
+				case _ if 5.0 > nota >= 3.0: rendimento[nome] = "MI"
+				case _ if 3.0 > nota > 0: rendimento[nome] = "II"
+				case _ if nota == 0: rendimento[nome] = "SR"
 			if self.__rep_faltas(nome):
-				self.notas["rendimento"] = "SR"
+				rendimento[nome] = "SR"
+		return rendimento
 
 
 su = simplNotas('notas1.txt', 'faltas1.txt')
+media = su.get_media({"P1": 1, "P2": 2, 'P3': 2, 'P4': 3, 'P5': 3})
+print(su.rendimento(media))
